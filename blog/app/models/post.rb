576a40b02d6bdc44a8c4c3c59fa7e8465d62366e
jonnorstrom <jon.norstrom@gmail.com>
2016-05-01 16:05:46 -0500

    add comments

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  # if post gets deleted, it's dependents get deleted also
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
